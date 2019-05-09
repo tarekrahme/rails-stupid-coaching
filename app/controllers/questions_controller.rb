1f9c8f73bea22be4f31f537c1e87735ba14874c9
Tarek Rahme <rahmetarek@gmail.com>
2019-05-09 14:47:58 +0100

    stupid coaching

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,11 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    @question = params['question']
+    @answer = 'I don\'t care, get dressed and go to work!'
+    @answer = 'Great' if @question == 'I am going to work'
+    @answer = 'Silly question, get dressed and go to work!' if @question[-1] == '?'
+  end
+end
